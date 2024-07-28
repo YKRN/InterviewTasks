5d3e452695673fff8dc132b0ed07a320de7b4934
Yavuz Karan <yavuz@karan.net.tr>
2024-07-28 14:49:52 +0300

    Update GlobalExceptionHandler.java

--- websocket_demo2/src/main/java/com/example/websocket_demo2/exception/GlobalExceptionHandler.java
+++ websocket_demo2/src/main/java/com/example/websocket_demo2/exception/GlobalExceptionHandler.java
@@ -1,4 +1,15 @@
 package com.example.websocket_demo2.exception;
 
 public class GlobalExceptionHandler {
+      @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public ResponseEntity<Map<String, Object>> handleException(Exception e) {
+        Map<String, Object> errorResponse = new HashMap<>();
+        errorResponse.put("timestamp", System.currentTimeMillis());
+        errorResponse.put("status", HttpStatus.BAD_REQUEST.value());
+        errorResponse.put("error", "Bad Request");
+        errorResponse.put("message", e.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+  
 }
